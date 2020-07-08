ad11a1fb3fb12cb3a2e5404a6c0e92626a06a6db
Andressa.licio <andressa_licio@hotmail.com>
2020-07-08 20:44:34 +0100

    altering backend url

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'https://student-api-andressa.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
